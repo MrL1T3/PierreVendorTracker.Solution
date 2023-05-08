f767e6bfe87feda7b31626b88a82a465bf3406f6
Emil Anton <emilbstn@gmail.com>
2023-05-08 12:24:53 -0700

    add VendorsTests.cs

--- PierreVendorTracker.Tests/ModelTests/VendorsTests.cs
+++ PierreVendorTracker.Tests/ModelTests/VendorsTests.cs
@@ -1,11 +1,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierreVendorTracker.Models;
+using System;
 
 namespace PierreVendorTracker.Tests
 {
   [TestClass]
   public class VendorsTests
   {
-
+  
   }
 }
