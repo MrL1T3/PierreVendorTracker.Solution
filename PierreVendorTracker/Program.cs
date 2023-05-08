1bb107af5f23708f0fbe181cca6351d804524ec7
Emil Anton <emilbstn@gmail.com>
2023-05-08 14:54:29 -0700

    add Main() method to run webpage

--- PierreVendorTracker/Program.cs
+++ PierreVendorTracker/Program.cs
@@ -5,8 +5,24 @@ namespace VendorOrderTracker
 {
   class Program
   {
-    static void Main()
+    static void Main(string[] args)
     {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddControllersWithViews();
+
+      WebApplication app = builder.Build();
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+
+      app.Run();
     }
   }
 }
