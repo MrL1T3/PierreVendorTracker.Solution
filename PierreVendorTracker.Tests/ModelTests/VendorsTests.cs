be532ecb5166ed9a6389acdf551603edfa71fcda
Emil Anton <emilbstn@gmail.com>
2023-05-08 12:27:26 -0700

    add first test

--- PierreVendorTracker.Tests/ModelTests/VendorsTests.cs
+++ PierreVendorTracker.Tests/ModelTests/VendorsTests.cs
@@ -7,6 +7,11 @@ namespace PierreVendorTracker.Tests
   [TestClass]
   public class VendorsTests
   {
-  
+    [TestMethod]
+    public void vendorName_Equals_John_True()
+    {
+      Vendor first = new Vendor("John", "");
+      Assert.AreEqual("John", first.VendorName);
+    }
   }
 }
