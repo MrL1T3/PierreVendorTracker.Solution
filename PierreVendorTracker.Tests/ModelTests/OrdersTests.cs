7722e98d7ae1b69d64d6b3b8fb115eca1fa1b184
Emil Anton <emilbstn@gmail.com>
2023-05-22 18:02:19 -0700

    add test for AddOrder method of Order class

--- PierreVendorTracker.Tests/ModelTests/OrdersTests.cs
+++ PierreVendorTracker.Tests/ModelTests/OrdersTests.cs
@@ -19,5 +19,15 @@ namespace PierreVendorTracker.Tests
       Order newOrder = new Order("", "Example", "", "", 1);
       Assert.AreEqual(1, newOrder.VendorID);
     }
+    [TestMethod]
+    public void AddOrder_Wood_NoDesc_NoDate_ThirteenDollars()
+    {
+      Order newOrder = new Order("Wood", "", "", "13 dollars", 0);
+      Vendor newVendor = new Vendor("Johnny", "Sells Chairs");
+      PierreVendorTracker.Models.Order.AddOrder(newOrder);
+      Console.WriteLine(newVendor.speOrders);
+      Assert.AreEqual("Wood", newVendor.speOrders[1].Title);
+      Assert.AreEqual("13 dollars", newVendor.speOrders[1].Price);
+    }
   }
 }
