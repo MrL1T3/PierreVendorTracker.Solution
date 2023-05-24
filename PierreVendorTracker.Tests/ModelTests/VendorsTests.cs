11db57bfac6f526042f6bedcc2d8e65c2d728a4a
Emil Anton <emilbstn@gmail.com>
2023-05-23 17:47:59 -0700

    move AddOrder test from OrderTests.cs to VendorTests.cs

--- PierreVendorTracker.Tests/ModelTests/VendorsTests.cs
+++ PierreVendorTracker.Tests/ModelTests/VendorsTests.cs
@@ -19,5 +19,21 @@ namespace PierreVendorTracker.Tests
       Vendor first = new Vendor("John", "John sells glassware products");
       Assert.AreEqual("John sells glassware products", first.VendorDesc);
     }
+    [TestMethod]
+    public void AddOrder_Wood_NoDesc_NoDate_ThirteenDollars()
+    {
+      Vendor newVendor = new Vendor("Johnny", "Sells Chairs");
+      Vendor.AddVendor(newVendor);
+
+      Console.WriteLine(newVendor.VendorID);
+      Order newOrder = new Order("Wood", "", "13 dollars", "", newVendor.VendorID);
+      Console.WriteLine(newOrder.VendorID);
+      
+      Vendor.AddOrder(newOrder);
+      Console.WriteLine(newVendor.speOrders);
+      Assert.AreEqual(1, newVendor.speOrders.Count);
+      Assert.AreEqual("Wood", newVendor.speOrders[0].Title);
+      Assert.AreEqual("13 dollars", newVendor.speOrders[0].Price);
+    }
   }
 }
